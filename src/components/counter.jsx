91c9a496f6d58ffdd39a471cd867860ceac7ebf5
ArjunSinghBhakuni <arjunbhakuni23@gmail.com>
2022-05-21 15:51:55 +0530

    done

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,5 +1,7 @@
 import React,{useState} from "react";
 
+ import Style from "./Counter.css"
+
 const Counter =()=>{
 
  const [count,setCount]= useState(0)
@@ -7,9 +9,10 @@ const Counter =()=>{
  return (
 
   <div>
-   <h1>Counter App : {count}</h1>
+   <h1 >Counter App : <span className={count%2===1 ? "red" : "green"}>  {count}</span> </h1>
    <button onClick={()=>setCount(count+1)}>Increament</button>
-<button onClick={()=>setCount(count-1)}>Decreaement</button>
+<button onClick={()=>
+  setCount(count-1)}>Decreaement</button>
 <button onClick={()=>setCount(count*2)}>Double</button>
   </div>
  )
